475d30af56a92036c2a3cfd1ffb92495b6f5c677
Steven Fuqua <steven@sariph.net>
2020-07-16 10:58:22 -0700

    Cleanup

--- module/src/loc.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loc = void 0;
-/**
- * Shortcut to localize a string key.
- * @param key Resource key to localize
- */
-exports.loc = (key) => game.i18n.localize(key);
